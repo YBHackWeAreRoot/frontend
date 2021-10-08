e8a524642af7a6720d188ceb5f0b89762b7c56c2
kasm <martin.kaeser@zuehlke.com>
2021-10-08 12:10:28 +0200

    First commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './components/app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MapComponent } from './app/components/map/map.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MapComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
