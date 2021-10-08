5f49fec06e534c4fd33fd2b765675bf17d0e16c5
Rolf Nyffenegger <rolf.nyffenegger@xovis.com>
2021-10-09 00:53:16 +0200

    take the reserve-time from filter object

--- src/app/components/parking-space-detail-dialog/parking-space-detail-sheet.component.ts
+++ src/app/components/parking-space-detail-dialog/parking-space-detail-sheet.component.ts
@@ -3,6 +3,7 @@ import {ParkingSpace} from "../../model/parking-space";
 import {MAT_BOTTOM_SHEET_DATA, MatBottomSheetRef} from "@angular/material/bottom-sheet";
 import {DatePipe} from "@angular/common";
 import {BookingService} from '../../services/booking.service';
+import {FilterService} from '../../services/filter.service';
 
 @Component({
   selector: 'app-parking-space-detail-dialog',
@@ -10,18 +11,25 @@ import {BookingService} from '../../services/booking.service';
   styleUrls: ['./parking-space-detail-sheet.component.scss']
 })
 export class ParkingSpaceDetailSheetComponent {
+  private fromDate?: Date;
+  private toDate?: Date;
 
   public constructor(
     private parkingSpaceDetailSheetMatBottomSheetRef: MatBottomSheetRef<ParkingSpaceDetailSheetComponent>,
     private readonly bookingService: BookingService,
+    private readonly filterService: FilterService,
     public date: DatePipe,
     @Inject(MAT_BOTTOM_SHEET_DATA) public data: ParkingSpace
   ) {
+    filterService.filterChangeObservable().subscribe(filter => {
+      this.fromDate = filter.from;
+      this.toDate = filter.to;
+    });
   }
 
   public reserve() {
-    if (this.data.id && this.data.fromTime && this.data.toTime) {
-      this.bookingService.createBooking(this.data.id, this.data.fromTime, this.data.toTime).subscribe(() => {
+    if (this.data.id && this.fromDate && this.toDate) {
+      this.bookingService.createBooking(this.data.id, this.fromDate, this.toDate).subscribe(() => {
         this.bookingService.triggerBookingsReload();
         this.parkingSpaceDetailSheetMatBottomSheetRef.dismiss();
       });
