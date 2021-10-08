ffbf6b4370780c95cc2b477b68194e9062fee0f6
Rolf Nyffenegger <rolf.nyffenegger@xovis.com>
2021-10-08 22:10:14 +0200

    added buttons in upcoming booking and rearranged

--- src/app/components/booking-dialog/booking-sheet.component.ts
+++ src/app/components/booking-dialog/booking-sheet.component.ts
@@ -1,40 +1,43 @@
-import {Component, Inject, OnInit} from '@angular/core';
+import {Component, Inject} from '@angular/core';
 import {MAT_BOTTOM_SHEET_DATA, MatBottomSheetRef} from "@angular/material/bottom-sheet";
 import {DatePipe} from "@angular/common";
 import {Booking, BookingStatus} from "../../model/booking";
 import {SelectParkingSpaceService} from '../../services/select-parking-space.service';
+import {BookingService} from '../../services/booking.service';
 
 @Component({
   selector: 'app-booking-sheet',
   templateUrl: './booking-sheet.component.html',
   styleUrls: ['./booking-sheet.component.scss']
 })
-export class BookingSheetComponent implements OnInit {
+export class BookingSheetComponent {
   public bookingStatusEnum = BookingStatus;
   public showCancelBooking = true;
   private showCheckOutButton = true;
+  public isCheckinPossible = true;
 
-  constructor(
+  public constructor(
     private bookingSheetMatBottomSheetRef: MatBottomSheetRef<BookingSheetComponent>,
     private selectParkingSpaceService: SelectParkingSpaceService,
+    private bookingService: BookingService,
     public date: DatePipe,
     @Inject(MAT_BOTTOM_SHEET_DATA) public data: Booking,
-  ) { }
-
-  ngOnInit(): void {
+  ) {
   }
 
-  cancelBooking() {
-    this.showCancelBooking = false;
-
+  public cancelBooking() {
+    this.bookingService.cancelBooking(this.data.id).subscribe(() => this.showCancelBooking = false);
   }
 
-  checkOut() {
-    this.showCheckOutButton = false;
-
+  public checkOut() {
+    this.bookingService.checkOut(this.data.id).subscribe(() => this.showCheckOutButton = false);
   }
 
   public showParkingLocationOnMap() {
     this.selectParkingSpaceService.goToParkingSpace.next(this.data.parkingSpace);
+  }
+
+  public checkIn() {
+    this.bookingService.checkIn(this.data.id).subscribe(() => this.isCheckinPossible = false);
   }
 }
