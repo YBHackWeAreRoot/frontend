224eb0c1e413ec90fdf51521bfe54c6cb5da6989
kasm <martin.kaeser@zuehlke.com>
2021-10-08 13:37:44 +0200

    First simple map

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -1,14 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import * as Leaflet from 'leaflet';
 
-export const getLayers = (): Leaflet.Layer[] => {
-  return [
-    new Leaflet.TileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; OpenStreetMap contributors'
-    } as Leaflet.TileLayerOptions),
-  ] as Leaflet.Layer[];
-};
-
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
@@ -21,7 +13,7 @@ export class MapComponent implements OnInit {
   title = 'AngularOSM';
 
   options: Leaflet.MapOptions = {
-    layers: getLayers(),
+    layers: MapComponent.getLayers(),
     zoom: 12,
     center: new Leaflet.LatLng(this.latitude, this.longitude)
   };
@@ -31,5 +23,13 @@ export class MapComponent implements OnInit {
   public ngOnInit(): void {
 
   }
+
+  private static getLayers(): Leaflet.Layer[] {
+    return [
+      new Leaflet.TileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; OpenStreetMap contributors'
+      } as Leaflet.TileLayerOptions),
+    ] as Leaflet.Layer[];
+  };
 
 }
